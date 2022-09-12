a4693147bccd73f71167cd27270ed21e3f3094ae
fridaySong3 <pleasure5567@gmail.com>
2022-09-12 20:28:27 +0900

    chore: Avoid printing detailed error message to prevent sql injection

--- src/error.rs
+++ src/error.rs
@@ -2,23 +2,43 @@ use diesel::result::{Error, DatabaseErrorKind};
 use rocket::http::Status;
 
 pub fn err_to_response(error: Error) -> (Status, String) {
-    let status = match error {
-        Error::NotFound => Status::NotFound,
-        Error::QueryBuilderError(_) => Status::UnprocessableEntity,
+    let status: Status;
+    let mut msg: &str = "";
+    match error {
+        Error::NotFound => {
+            status = Status::NotFound;
+        }
+        Error::QueryBuilderError(_) => {
+            status = Status::UnprocessableEntity;
+            msg = "The query could not be constructed. \nAn example of when this error could occur is if you are attempting to construct an update statement with no changes (e.g. all fields on the struct are None).";
+        }
         Error::DatabaseError(kind, ref info) => {
             match kind {
-                DatabaseErrorKind::UniqueViolation => Status::Conflict,
-                DatabaseErrorKind::ForeignKeyViolation => Status::UnprocessableEntity,
+                DatabaseErrorKind::UniqueViolation => {
+                    status = Status::Conflict;
+                    msg = "A unique constraint was violated.";
+                }
+                DatabaseErrorKind::ForeignKeyViolation => {
+                    status = Status::UnprocessableEntity;
+                    msg = "A foreign key constraint was violated.";
+                }
                 _ => {
                     match info.constraint_name() {
-                        Some(_) => Status::UnprocessableEntity,
-                        None => Status::InternalServerError
+                        Some(constraint_name) => {
+                            status = Status::UnprocessableEntity;
+                            msg = constraint_name;
+                        }
+                        None => {
+                            status = Status::InternalServerError;
+                        }
                     }
                 }
             }
         }
-        _ => Status::InternalServerError,
+        _ => {
+            status = Status::InternalServerError;
+        }
     };
     
-    (status, format!("{}: {}", status.code.to_string(), error.to_string()))
+    (status, format!("{} {}\n{}", status.code.to_string(), status.reason().unwrap_or(""), msg))
 }
